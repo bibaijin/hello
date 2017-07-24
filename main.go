5b4ad41441c3a2f8db76a0b7fbbeed9dcb71831b
bibaijin <bibaijin@gmail.com>
2017-07-24 13:58:16 +0800

    use zap

--- main.go
+++ main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"crypto/rand"
+	"encoding/hex"
 	"log"
 	"net/http"
-	"os"
-)
 
-// LogFlag 控制日志的前缀
-const LogFlag = log.LstdFlags | log.Lmicroseconds | log.Lshortfile
-
-var (
-	errLogger  = log.New(os.Stderr, "ERROR ", LogFlag)
-	infoLogger = log.New(os.Stdout, "INFO ", LogFlag)
+	"go.uber.org/zap"
 )
 
 func main() {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		infoLogger.Printf("Receive a ping request.")
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("zap.NewProduction() failed, error: %v.", err)
+	}
+	defer logger.Sync()
 
-		if _, err := fmt.Fprintf(w, "OK"); err != nil {
-			errLogger.Printf("fmt.Fprintf failed, error: %s.", err)
-		}
-
-		infoLogger.Printf("Response is sent.")
-	})
+	http.HandleFunc("/test", handle(test, logger))
+	http.HandleFunc("/ping", handle(ping, logger))
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("http.ListenAndServe failed, error: %s.", err)
+		logger.Fatal("http.ListenAndServe() failed.",
+			zap.String("address", ":8080"),
+			zap.Error(err),
+		)
 	}
+}
+
+func test(w http.ResponseWriter, r *http.Request, logger *zap.Logger) {
+}
+
+func ping(w http.ResponseWriter, r *http.Request, logger *zap.Logger) {
+	if _, err := w.Write([]byte("OK")); err != nil {
+		logger.Error("w.Write([]byte(\"OK\")) failed.", zap.Error(err))
+	}
+}
+
+func handle(f func(w http.ResponseWriter, r *http.Request, logger *zap.Logger), logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		newLogger := logger.With(zap.String("RequestID", newRequestID()))
+		newLogger.Info("Receive a new request.",
+			zap.String("RemoteAddr", r.RemoteAddr),
+			zap.String("Method", r.Method),
+			zap.String("URL", r.URL.String()),
+			zap.Any("Header", r.Header),
+		)
+		f(w, r, newLogger)
+		newLogger.Info("Response is sent.",
+			zap.String("RemoteAddr", r.RemoteAddr),
+			zap.String("Method", r.Method),
+			zap.String("URL", r.URL.String()),
+			zap.Any("Header", r.Header),
+		)
+	}
+}
+
+func newRequestID() string {
+	bs := make([]byte, 16)
+	if _, err := rand.Read(bs); err != nil {
+		return "0"
+	}
+
+	return hex.EncodeToString(bs)
 }
