4dface1298bac012926f546bfdbf4dda7b73bb5f
bibaijin <bibaijin@gmail.com>
2017-07-24 16:44:01 +0800

    use s6 image

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"log"
 	"net/http"
+	"os"
 
 	"go.uber.org/zap"
 )
@@ -16,7 +17,6 @@ func main() {
 	}
 	defer logger.Sync()
 
-	http.HandleFunc("/test", handle(test, logger))
 	http.HandleFunc("/ping", handle(ping, logger))
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -27,13 +27,14 @@ func main() {
 	}
 }
 
-func test(w http.ResponseWriter, r *http.Request, logger *zap.Logger) {
-}
-
 func ping(w http.ResponseWriter, r *http.Request, logger *zap.Logger) {
 	if _, err := w.Write([]byte("OK")); err != nil {
 		logger.Error("w.Write([]byte(\"OK\")) failed.", zap.Error(err))
 	}
+
+	file, _ := os.OpenFile("/var/log/test/test.out", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	file.Write([]byte("OK.\n"))
+	file.Close()
 }
 
 func handle(f func(w http.ResponseWriter, r *http.Request, logger *zap.Logger), logger *zap.Logger) http.HandlerFunc {
